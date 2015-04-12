58a0c2d1224756decb47384532686ae3a0116531
demastri <gear@demastri.com>
2015-04-12 16:06:59 -0500

    add worker handle...

---
+++ ComputeFarmProxy/WorkerHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QueueCommon;
+
+namespace ComputeFarmProxy
+{
+    public class WorkerHandle
+    {
+        Queue refQueue;
+        string refTypeID;
+        public Queue worker { get { return refQueue; } }
+        public string typeID { get { return refTypeID; } }
+
+        public WorkerHandle(Queue thisQueue, string thisTypeID)
+        {
+            refQueue = thisQueue;
+            refTypeID = thisTypeID;
+        }
+    }
+}
